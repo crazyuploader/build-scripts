42f22036cc6fb40ef53802c126e0f3bd84fee0df
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-11-04 12:56:15 +0530

    Update cpp.sh

--- cpp.sh
+++ cpp.sh
@@ -3,6 +3,7 @@ for f in $(ls *.cpp); do echo "Compiling with g++ $f"; g++ $f; done
 for f in $(ls Practical_File/*.cpp); do echo "Compiling with g++ $f"; g++ $f; done
 echo ""
 echo "Build with G++ OK"
+echo ""
 echo "Compiling with Clang++"
 for f in $(ls *.cpp); do echo "Compiling with clang++ $f"; clang++ $f; done
 for f in $(ls Practical_File/*.cpp); do echo "Compiling with clang++ $f"; clang++ $f; done
