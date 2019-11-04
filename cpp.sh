6bdc30c2331932f536cbf6cbe4697f93e8cbd8ac
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-11-04 13:19:18 +0530

    Update cpp.sh

--- cpp.sh
+++ cpp.sh
@@ -1,13 +1,11 @@
-LGR='\033[1;32m'
-
 echo "Compiling with G++"
 for f in $(ls *.cpp); do echo "Compiling with g++ $f"; g++ $f; done
 for f in $(ls Practical_File/*.cpp); do echo "Compiling with g++ $f"; g++ $f; done
 echo ""
-echo -e ${LGR} "Build with G++ OK"
+echo "Build with G++ OK"
 echo ""
 echo "Compiling with Clang++"
 for f in $(ls *.cpp); do echo "Compiling with clang++ $f"; clang++ $f; done
 for f in $(ls Practical_File/*.cpp); do echo "Compiling with clang++ $f"; clang++ $f; done
 echo ""
-echo -e ${LGR} "Build with Clang++ OK"
+echo "Build with Clang++ OK"
