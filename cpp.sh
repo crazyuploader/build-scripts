66333b0293b37210ddf0ad5709774162280c2cb5
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-12-08 15:21:40 +0530

    Update cpp.sh

--- cpp.sh
+++ cpp.sh
@@ -1,11 +1,18 @@
-echo "Compiling with G++"
-for f in $(ls *.cpp); do echo "Compiling with g++ $f"; g++ $f; done
-for f in $(ls Practical_File/*.cpp); do echo "Compiling with g++ $f"; g++ $f; done
 echo ""
-echo "Build with G++ OK"
+echo "Compiling with G++"
+echo ""
+for f in $(ls *.cpp); do echo "Compiling $f"; g++ $f; done
+echo ""
+echo "Compiling Practical_File/*"
+echo ""
+for f in $(ls Practical_File/*.cpp); do echo "Compiling $f"; g++ $f; done
+echo ""
 echo ""
 echo "Compiling with Clang++"
-for f in $(ls *.cpp); do echo "Compiling with clang++ $f"; clang++ $f; done
-for f in $(ls Practical_File/*.cpp); do echo "Compiling with clang++ $f"; clang++ $f; done
 echo ""
-echo "Build with Clang++ OK"
+for f in $(ls *.cpp); do echo "Compiling $f"; clang++ $f; done
+echo ""
+echo "Compiling Practical_File/*"
+echo ""
+for f in $(ls Practical_File/*.cpp); do echo "Compiling $f"; clang++ $f; done
+echo ""
